349ffc3a7562956fe4f858e6a529cc9980f4504b
loickcherimont <loickcherimont@gmail.com>
2024-08-27 23:34:24 +0200

    Redesign the app, modify phone image name and finish README.md

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,66 @@
-<template>
+<template class="">
   <!-- Header -->
-  <h1 class="">E-Shop 🛒</h1>
-  <main>
-
+  <h1 class="bg-emerald-600 text-white font-extrabold text-5xl text-center mb-3 p-4">MyE-Shop 🛒</h1>
+  <main class="flex flex-col">
+    <!-- Place for bought products -->
+    <Cart :cart="cart" :getTotal="getTotal" :getPrice="getPrice" />
     <!-- Products Store -->
-    <div class="products" :style="{ display: 'flex', justifyContent: 'space-around' }">
+    <div class="products flex flex-wrap justify-center gap-10">
 
-      <Product 
-        v-for="product in products"
-        :key="product.id"
-        v-bind="product"
-        :add-to-cart="addToCart"
-        :remove-from-cart="removeFromCart"
-      />
+      <Product v-for="product in products" :key="product.id" v-bind="product" :add-to-cart="addToCart"
+        :remove-from-cart="removeFromCart" />
 
     </div>
-
-    <!-- Place for bought products -->
-    <Cart 
-      :cart="cart"
-      :getTotal="getTotal"
-      :getPrice="getPrice"
-    />
   </main>
+  <!-- Classic footer with &copy; -->
+  <footer class="text-slate-400 text-center p-4">
+    <p>&copy; 2024 - Loick Cherimont</p>
+  </footer>
 </template>
 
 <script setup>
-// -- All imports
 import { ref, onMounted, computed, watch } from 'vue';
-
-// todo: Re-use after debugging
-
 import Product from './components/Product.vue';
 import Cart from './components/Cart.vue';
 
-// -- Reference & reactives
+/** -- Usage -- */
 const products = ref(null);
 const cart = ref([]);
 
-// -- Lifecycle hooks
-// todo: Review + how to write correctly json
-// todo: Fetch products from ./products.json
 onMounted(fetchData);
 
+/** Prevent products <ref> to mutate */
+watch((products), fetchData)
 
-// -- Computed
-// Computed for total quantity / price
+/**
+ * Returns total quanity.
+ * 
+ * @function getTotal
+ * @returns {number} Total number of products that user buys.
+ */
 const getTotal = computed(() => {
-  return cart.value.reduce((acc, product) => acc + product.quantity , 0);
+  return cart.value.reduce((acc, product) => acc + product.quantity, 0);
 })
 
+/**
+ * Returns total price.
+ * 
+ * @function getPrice
+ * @returns {number} Total cost that user has to pay.
+ */
 const getPrice = computed(() => {
   return cart.value.reduce((acc, product) => acc + product.price, 0);
 })
 
-// -- Watchers
-// Prevent products to mutate?
-watch((products), fetchData)
 
-// -- Custom functions
+/** -- Functions -- */
+
+/**
+* Fetch products from products.json'
+* Throw an error if cannot get data
+*
+* @async fetchData
+*/
 async function fetchData() {
   const response = await fetch('./src/assets/products.json', {
     method: 'GET',
@@ -73,18 +75,26 @@ async function fetchData() {
   products.value = data;
 }
 
-// todo : Re-see how to create a cart in Vue.js
+/**
+* Add a product into the cart by id.
+*
+* @function addToCart
+* @param {number} id - Id of product to add
+*/
 function addToCart(id) {
   let product = products.value.find(p => p.id === id);
 
-  // New product to add
-  // Create a quantity prop.
+  /** Product does not exists 
+   *  Add it to cart with a new quantity prop. initialized to 1 
+   */
   if (!cart.value.find(p => p.id === id)) {
     product.quantity = 1;
     cart.value.push(product);
-  
-  // User selected same product
-  // Update price and quantity so in cart
+
+    /**
+     * User selected a product ever in cart
+     * Update price and quantity props.
+     */
   } else {
     const productCart = cart.value.find(p => p.id === id);
     productCart.quantity++;
@@ -92,18 +102,27 @@ function addToCart(id) {
   }
 }
 
+/**
+* Remove a product from cart by id.
+*
+* @function removeFromCart
+* @param {number} id - Id of product to remove.
+*/
 function removeFromCart(id) {
   let product = products.value.find(p => p.id === id);
   const productToRemove = cart.value.find(p => p.id === id);
 
   switch (true) {
-    case !productToRemove: // do not anything if no product
+    /** No product, nothing special. */
+    case !productToRemove: // 
       return;
+    /** More than one, decrement quantity/price props. */
     case productToRemove.quantity > 1:
       productToRemove.quantity--;
       productToRemove.price -= product.price;
       break;
-    default: // there is 1 product only, delete it from cart
+    /** There is 1 product only, delete it from cart */
+    default:
       cart.value = cart.value.filter(p => p.id !== id);
   }
 }
--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -1,15 +1,14 @@
 <template>
-    <div class="cart">
-        <h2>Your Cart</h2>
-        <h3>Products</h3>
+    <div class="cart flex flex-col justify-center items-center">
+        <h2 class="text-slate-700 font-bold text-3xl">Your Cart</h2>
         <template v-if="cart.length === 0">
-            NO PRODUCT
+            <p class="text-slate-600 italic">NO PRODUCT</p>
         </template>
         <template v-else>
-            <p>{{ getTotal }} Product{{ (getTotal < 1) ? '' : 's' }} | {{ getPrice.toFixed(2) }}€</p>
+            <h3 class="text-slate-600 text-2xl font-bold">{{ getTotal }} Product{{ (getTotal <= 1) ? '' : 's' }}<span class="text-black"> |</span> <span class="text-emerald-500">{{ getPrice.toFixed(2) }}€</span></h3>
                     <ul>
                         <li v-for="product in cart">
-                            {{ product.name }} x {{ product.quantity }} - {{ product.price.toFixed(2) }}€
+                            {{ product.name }} x {{ product.quantity }} - <span class="text-emerald-500">{{ product.price.toFixed(2) }}€</span>
                         </li>
                     </ul>
         </template>
@@ -19,7 +18,7 @@
 <script setup>
 defineProps({
     cart: Array,
-    getTotal: Function,
-    getPrice: Function
+    getTotal: Number,
+    getPrice: Number
 })
 </script>
